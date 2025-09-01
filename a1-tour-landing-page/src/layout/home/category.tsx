716e96732ffa6c7c0708558114df61040b066d50
ldanh270 <ducanhle.dn@gmail.com>
2025-09-01 15:51:44 +0700

    feat(category): add category title

--- a1-tour-landing-page/src/layout/home/category.tsx
+++ a1-tour-landing-page/src/layout/home/category.tsx
@@ -1,3 +1,11 @@
 export default function Category() {
-    return <div></div>
+    return (
+        <div className="w-full flex flex-col justify-start items-center">
+            <div id="title">
+                <div className="font-accent text-4xl text-center">Wornderful Place For You</div>
+                <div className="font-title font-bold text-5xl text-center">Tour Categories</div>
+            </div>
+            <div id="slider"></div>
+        </div>
+    )
 }
